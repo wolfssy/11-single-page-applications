4e6bfaf5e857932936cc22592b5daec50af19f0d
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-08-28 14:46:07 -0700

    some todo's done

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,10 +3,18 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // TODO:(?) Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
 
-console.log('over it');
+
+    articleView.handleMainNav = function() {
+        $('.main-nav').on('click', '.tab', function() {
+          $('.tab-content').hide();
+          $(`#${$(this).data('content')}`).fadeIn();
+        });
+
+       $('.main-nav .tab:first').click();
+      };
   module.articleController = articleController;
 })(app);
